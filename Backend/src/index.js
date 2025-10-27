6479526eb177b34a4c85f3c95ac78bc3dae96da0
Mohit Godara <kukuchoudhary816@gmail.com>
2025-10-27 10:54:44 +0530

    work on deplyo

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -5,15 +5,13 @@ import DB from "./db/connect.db.js";
 import cors from "cors";
 
 
-app.use(express.json());
-
+const PORT = process.env.PORT||5000;
 app.use(cors({
-  origin: ["https://apnidukanadmin.netlify.app"],
+  origin: ["https://apni-dukan-admin-omega.vercel.app",`http://localhost:${PORT}`],
   methods: ["GET", "POST", "PUT", "DELETE"],
   credentials: true
 }));
 DB().then(()=>{
-    const PORT = process.env.PORT||5000;
     app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
 }
 ).catch(
